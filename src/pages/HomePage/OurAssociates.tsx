50b9adeb73690e4189697062485ff648a6e9dd74
SalehAhmed19 <saleh.ahmed.mahin@gmail.com>
2026-02-07 16:21:55 +0600

    update

--- src/pages/HomePage/OurAssociates.tsx
+++ src/pages/HomePage/OurAssociates.tsx
@@ -1,15 +1,20 @@
 import AssociatesCard from "../../components/ui/Cards/AssociatesCard";
 import Heading from "../../components/ui/Headings/Heading";
 import { useGetAssociates } from "@/hooks/useGetAssociates";
-import { BlogsSkeleton } from "@/components/ui/Skeletons/BlogsSkeleton";
 import { motion } from "framer-motion";
 import { ourAssociatesVariants } from "@/motions/motions";
+import { useEffect, useState } from "react";
 
 export default function OurAssociates({ sliceNumber }: { sliceNumber?: number }) {
     const { associates, loading } = useGetAssociates()
-    const loadingArr = [
-        1, 2, 3, 4, 5
-    ]
+    const [staticAssociate, setStaticAssociate] = useState([]);
+
+    useEffect(() => {
+        fetch("/shamiul-islam-and-associates.associates.json")
+            .then((res) => res.json())
+            .then((data) => setStaticAssociate(data))
+    }, [])
+
 
     if (loading) return <section className="py-12 md:py-16 px-4 md:px-8 bg-[#FAF9F4] space-y-8 md:space-y-10">
         <div className="space-y-2">
@@ -20,18 +25,32 @@ export default function OurAssociates({ sliceNumber }: { sliceNumber?: number })
 
         </div>
 
-        {/* Grid is already good, just added 'max-w-7xl' to prevent stretching on huge screens */}
         <div className="flex justify-center">
-            {loadingArr.slice(0, 1).map((associate) => (
-                <BlogsSkeleton key={associate} />
+            {staticAssociate?.slice(0, 1).map((associate: any, index: number) => (
+                <AssociatesCard
+                    key={index}
+                    image={associate.image?.url as string}
+                    name={associate.name}
+                    court={associate.court}
+                    position={associate.position}
+                // socialLinks={associate.socialLinks}
+                />
             ))}
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 container mx-auto max-w-7xl">
-            {loadingArr.slice(1, sliceNumber ? sliceNumber : associates.length).map((associate) => (
-                <BlogsSkeleton key={associate} />
+            {staticAssociate?.slice(1, sliceNumber ? sliceNumber : associates.length).map((associate: any, index: number) => (
+                <AssociatesCard
+                    key={index}
+                    image={associate.image?.url as string}
+                    name={associate.name}
+                    court={associate.court}
+                    position={associate.position}
+                // socialLinks={associate.socialLinks}
+                />
             ))}
         </div>
     </section >
+    console.log({ staticAssociate })
 
     return (
         <section className="py-12 md:py-16 px-4 md:px-8 bg-[#FAF9F4] space-y-8 md:space-y-10">
